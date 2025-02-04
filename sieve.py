0d5230302b5518870614b2d7c8fa2c6f9eb03a5c
Philémon van Helden <philemon.vanhelden@gmx.com>
2025-02-04 13:32:43 +0100

    sieve improved

--- sieve.py
+++ sieve.py
@@ -1,64 +1,41 @@
 def sieve(limit):
+    # We know 2 and 3 to be prime
+    res = []
     if limit < 2:
-        return []
-
+        return res  # []
+    res.extend([False, True])
     if limit == 2:
-        return [False, True]
-
+        return res  # [False, True]
+    res.append(True)
     if limit == 3:
-        return [False, True, True]
+        return res  # [False, True, True]
 
-    res = [False]
-
-    if limit >= 2:
-        res[2] = True
-
-    if limit >= 3:
-        res[3] = True
-
-    for i in range(4, limit + 1):
-        res[i] = False
+    res.extend([False] * (limit - 2))
 
     i = 1
-
-    while i <= limit:
-
+    while i * i <= limit:
         j = 1
-
-        while j <= limit:
-
+        while j * j <= limit:
             n = (4 * i * i) + (j * j)
-
-            if (n <= limit and (n % 12 == 1 or
-
-                                n % 12 == 5)):
+            if n <= limit and (n % 12 == 1 or n % 12 == 5):
                 res[n] ^= True
 
             n = (3 * i * i) + (j * j)
-
             if n <= limit and n % 12 == 7:
                 res[n] ^= True
 
             n = (3 * i * i) - (j * j)
-
-            if (i > j and n <= limit and
-
-                    n % 12 == 11):
+            if i > j and n <= limit and n % 12 == 11:
                 res[n] ^= True
 
             j += 1
-
         i += 1
 
     r = 5
-
     while r * r <= limit:
-
         if res[r]:
-
             for i in range(r * r, limit + 1, r * r):
                 res[i] = False
-
         r += 1
 
     return res
@@ -66,41 +43,35 @@ def sieve(limit):
 
 def pick_prime(primes, min_size=1000):
     """returns a suitable prime to use as modulus"""
-
-    for prime in primes:
-
-        if prime >= min_size:
-            return prime
-
     # if no prime large enough exists, use last one on list
+    if len(primes) <= min_size:
+        return len(primes)
 
-    return primes[-1]
+    for i in range(len(primes)):
+        if i >= min_size:
+            return i
 
 
-def hash(string, modulus):
+def hash_string(string, modulus):
     """implements polynomial rolling of string keys"""
-
     hash_value = 5381
-
     for char in string:
         # hash = 33 XOR ord(c)
-
         hash_value = ((hash_value << 5) + hash_value) ^ ord(char)
 
     return hash_value % modulus
 
 
-if __name__ == '__main__':
-
+def main():
     # generate primes list to use as modulus
-
     primes = sieve(10000)  # modify limit based on your needs
-
     modulus = pick_prime(primes, 1000)
 
     test_array = ["alpha", "beta", "gamma", "delta", "epsilon"]
-
     for string in test_array:
-        hash_value = hash(string, modulus)
-
+        hash_value = hash_string(string, modulus)
         print(f"Hash of {string} is {hash_value}")
+
+
+if __name__ == '__main__':
+    main()
